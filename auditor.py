b61a379019dde8ad6db4c4d4a9618578e58945da
Xin Yuan <2604239@sit.singaporetech.edu.sg>
2026-09-15 11:31:58 +0800

    inventory initialise 0

--- auditor.py
+++ auditor.py
@@ -1,6 +1,1 @@
-#inventory set 0 , failedreject set to 500
-#while inventory below 500, 
-# enter and add stock quantity to inventory
-
-#if "quit", show full quantity and failed rejected
-#else, continue with loop
+inventory=0 #type int
