834c3c9dd4d7998f1b0081069f9287444babe148
Xin Yuan <2604239@sit.singaporetech.edu.sg>
2026-09-15 16:14:40 +0800

    if else statement

--- auditor.py
+++ auditor.py
@@ -1,9 +1,17 @@
 
 
 inventory=0 #type int
+FailedReject=0
+
 while True:
     inventoryadd=input("Enter stock quantity:" )
     if inventoryadd == "quit":
         print("Total units processed is:", inventory, " Number of entries failed or rejected:", failedReject)
         break
-    elif 
+    elif inventoryadd.isdigit()== False:
+        print("Invalid input, please enter positive numbers")
+        failedReject=failedReject + 1
+    else:
+        inventoryadd=int(inventoryadd)
+        inventory=inventory + inventoryadd
+        print("Current total inventory:", inventory)
