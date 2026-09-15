f999d9f3ef95a67d768c3942e894b08693616dfc
Xin Yuan <2604239@sit.singaporetech.edu.sg>
2026-09-15 16:12:02 +0800

    if input is quit

--- auditor.py
+++ auditor.py
@@ -3,4 +3,7 @@
 inventory=0 #type int
 while True:
     inventoryadd=input("Enter stock quantity:" )
-        
+    if inventoryadd == "quit":
+        print("Total units processed is:", inventory, " Number of entries failed or rejected:", failedReject)
+        break
+    elif 
