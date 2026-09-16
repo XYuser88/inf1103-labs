1e0536f9937678a658bfadfec8f36d13ce1fa6e9
Xin Yuan <2604239@sit.singaporetech.edu.sg>
2026-09-16 10:06:49 +0800

    misc

--- auditor.py
+++ auditor.py
@@ -1,3 +1,10 @@
+#logic to audit inventory and reject invalid entries
+#inventory set 0 , failedreject set to 500
+#while inventory below 500, 
+# enter and add stock quantity to inventory
+
+#if "quit", show full quantity and failed rejected
+#else, continue with loop
 
 maxinventory=500
 inventory=0 #type int
