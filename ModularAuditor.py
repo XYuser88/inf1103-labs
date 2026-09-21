dd1ac08421ff7e4baf877d2fc21c352b80b7d2fd
Xin Yuan <2604239@sit.singaporetech.edu.sg>
2026-09-21 19:23:38 +0800

    main program

--- ModularAuditor.py
+++ ModularAuditor.py
@@ -10,25 +10,6 @@ maxinventory=500
 inventory=0 #type int
 failedReject=0
 FinalExceed=0
-#while True:
-    #inventoryadd=input("Enter stock quantity:" )
-    #if inventoryadd == "quit":
-        #print("Total units processed is:", inventory, " Number of entries failed or rejected:", failedReject)
-        #break
-    #elif inventoryadd.isdigit()== False:
-        #print("Invalid input, please enter positive numbers")
-        #failedReject=failedReject + 1
-    #else:
-        #inventoryadd=int(inventoryadd)
-        #inventory=inventory + inventoryadd
-        #print("Current total inventory:", inventory)
-
-    #if inventory>maxinventory:
-        #FinalExceed =inventory-maxinventory
-        #print("Inventory has exceeded maximum threshold by:",FinalExceed, " units")
-        #break
-    #else:
-        #continue
 
 def get_input():
         inventoryadd=input("Enter stock quantity:" )
@@ -36,7 +17,8 @@ def get_input():
             return "quit"
         elif inventoryadd.isdigit()==False:
             return None
-        return int(inventoryadd)
+        else:
+            return int(inventoryadd)
 
 def process_delivery(current_total, new_amt):
     new_total = current_total + new_amt
@@ -49,3 +31,31 @@ def calculate_tax(amount):
 def generate_report(total_units, failed_attempts):
     print("Total units processed is:", total_units)
     print("Number of entries failed or rejected:", failed_attempts)
+
+
+
+
+
+
+#main program
+while True:
+    inventoryadd=get_input()
+    if inventoryadd == "quit":
+        generate_report(inventory, failedReject)
+        break
+    elif inventoryadd.isdigit()== None:
+        print("Invalid input, please enter positive numbers")
+        failedReject=failedReject + 1
+    else:
+        inventory=process_delivery(inventory, inventoryadd)
+        tax=calculate_tax(inventoryadd)
+        print("Current total inventory:", inventory)
+        print("Tax on this delivery is:", tax)
+
+    if inventory>maxinventory:
+        FinalExceed =inventory-maxinventory
+        print("Inventory has exceeded maximum threshold by:",FinalExceed, " units")
+        break
+    else:
+        continue
+
